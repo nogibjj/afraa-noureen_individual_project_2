e3e0f431d1ac47bae556fa82ca015ab71b0cf624
afraa-n <143756865+afraa-n@users.noreply.github.com>
2023-10-30 21:06:32 +0000

    changes

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use afraa-n-baskin-robbins::{extract, query, transform_load};
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} [action]", args[0]);
+        return;
+    }
+
+    let action = &args[1];
+    match action.as_str() {
+        "extract" => {
+            extract(
+                "https://raw.githubusercontent.com/prasertcbs/basic-dataset/master/baskin_icecream.csv",
+                "data",
+            );
+        }
+        "transform_load" => match transform_load("data/baskin_icecream.csv") {
+            Ok(_) => println!("Data loaded successfully!"),
+            Err(err) => eprintln!("Error: {:?}", err),
+        },
+        "query" => {
+            if let Some(q) = args.get(2) {
+                if let Err(err) = query(q) {
+                    eprintln!("Error: {:?}", err);
+                } else {
+                    println!("Query executed successfully!");
+                }
+            } else {
+                println!("Usage: {} query [SQL query]", args[0]);
+            }
+        }
+        _ => {
+            println!("Invalid action. Use 'extract', 'transform_load', or 'query'.");
+        }
+    }
+}
