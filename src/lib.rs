3afa9707d562cb8501ccec19f5a6d4a115341237
GitHub Action <action@github.com>
2023-10-30 21:44:24 +0000

    Add query log

--- src/lib.rs
+++ src/lib.rs
@@ -70,14 +70,8 @@ pub fn transform_load(dataset: &str) -> Result<String> {
         match result {
             Ok(record) => {
                 stmt.execute([
-                    &record[0],
-                    &record[1],
-                    &record[2],
-                    &record[3],
-                    &record[4],
-                    &record[5],
-                    &record[6],
-                    &record[7],
+                    &record[0], &record[1], &record[2], &record[3], &record[4], &record[5],
+                    &record[6], &record[7],
                 ])?;
             }
             Err(err) => {
